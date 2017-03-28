a0e667fe3a6d4f1addc74084d9c630b55044033a
hecomi <info@hecomi.com>
2017-03-29 00:25:20 +0900

    add hue control sample.

---
+++ Assets/Holo_Hue_Control/HueColorChanger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using HoloToolkit.Unity.InputModule;
+
+public class HueColorChanger : MonoBehaviour, IInputClickHandler 
+{
+    public string apiUrlTemplate = "{server}/hue/{id}/{api}/{color}";
+    public string server = "http://192.168.1.2:3000";
+    public int id = 0;
+    public string api = "rgb";
+    public Color32 color = Color.white;
+
+    private string apiUrl
+    {
+        get 
+        { 
+            var colorStr = string.Format("{0},{1},{2}", color.r, color.g, color.b);
+            return apiUrlTemplate
+                .Replace("{server}", server)
+                .Replace("{id}", id.ToString())
+                .Replace("{api}", api)
+                .Replace("{color}", colorStr);
+        }
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        StartCoroutine(CallApi());
+    }
+
+    IEnumerator CallApi()
+    {
+        var req = UnityWebRequest.Get(apiUrl);
+        yield return req.Send();
+
+        if (req.isError) {
+            Debug.LogError(req.error);
+        }
+    }
+}
---
+++ Assets/Holo_Hue_Control/HueItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+[RequireComponent(typeof(Animator))]
+public class HueItem : MonoBehaviour, IInputClickHandler
+{
+    Animator animator_;
+    bool selected_ = false;
+    float previousEventTime = 0f;
+
+    void Start()
+    {
+        animator_ = GetComponent<Animator>();
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (Time.time - previousEventTime < 0.2f) return;
+        previousEventTime = Time.time;
+
+        selected_ = !selected_;
+        animator_.SetBool("selected", selected_);
+    }
+}
